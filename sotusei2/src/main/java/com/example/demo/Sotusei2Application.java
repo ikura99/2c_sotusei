f847591831acbff62f083069362393de5e4c8674
ikura99 <takahashirinto220@gmail.com>
2023-11-16 15:12:27 +0900

    11/16　javascriptの変更

--- sotusei2/src/main/java/com/example/demo/Sotusei2Application.java
+++ sotusei2/src/main/java/com/example/demo/Sotusei2Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SotuseiApplication {
+public class Sotusei2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SotuseiApplication.class, args);
+		SpringApplication.run(Sotusei2Application.class, args);
 	}
 
 }
