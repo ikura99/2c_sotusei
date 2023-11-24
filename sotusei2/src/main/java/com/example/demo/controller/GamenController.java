fb0720f98fa4762378115335b0d26e7f93ef74ae
teradakoki <teradakoukisitianhaogui@gmail.com>
2023-11-24 09:44:57 +0900

    確認

--- sotusei2/src/main/java/com/example/demo/controller/GamenController.java
+++ sotusei2/src/main/java/com/example/demo/controller/GamenController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
@@ -10,5 +11,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 		@RequestMapping(path = "/nyuuryoku", method = RequestMethod.GET)
 		public String nyuuryoku() {
 			return "nyuuryoku";
+		}
+		
+		@RequestMapping(path = "/syuturyoku", method = RequestMethod.POST)
+		public String syuturyoku(String departure,String destination, Model model) {
+			model.addAttribute("syutu", departure);	
+			model.addAttribute("moku", destination);	
+			return "syuturyoku";
 		}
 }
