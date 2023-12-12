f2d4cee08e6c8eb10f1cce92ff5809e3c8a2c879
ikura99 <takahashirinto220@gmail.com>
2023-12-12 13:52:26 +0900

    12/12

--- sotusei2/src/main/java/com/example/demo/controller/MyMapController.java
+++ sotusei2/src/main/java/com/example/demo/controller/MyMapController.java
@@ -1,7 +1,10 @@
 package com.example.demo.controller;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -30,21 +33,7 @@ public class MyMapController {
 	
 	
 	
-	//最初のページ
-		@RequestMapping(path = "/mymap2", method = RequestMethod.GET)
-		public String kari1() {
-			return "renshuu2";
-		}
 	
-	@RequestMapping(path = "/kaisi", method = RequestMethod.POST)
-	public String kari2(String start, Model model) {
-		model.addAttribute("start", start);	
-		System.out.println(start);
-		return "renshuu3";
-		
-	}
-
-
 
 
 
@@ -56,16 +45,29 @@ public class MyMapController {
 	}
 
 @RequestMapping(path = "/kaisi3", method = RequestMethod.POST)
-public String karikari2(String start, String goal,Model model) {
+public String karikari2(String start, String goal,Model model, @RequestBody Map<String, String> formData) {
 	model.addAttribute("start", start);	
 	model.addAttribute("goal", goal);	
+	model.addAttribute("formData", formData);
 	System.out.println(start+goal);
 	return "renshuu4";
 	
 }
 
 
+@RequestMapping(path = "/mymap4", method = RequestMethod.GET)
+public String karikari8888() {
+	return "renshuu6";
+}
 
+@RequestMapping(path = "/kaisi4", method = RequestMethod.POST)
+public String karikari28888(String start, String goal,Model model) {
+model.addAttribute("start", start);	
+model.addAttribute("goal", goal);	
+System.out.println(start+goal);
+return "renshuu5";
+
+}
 
 
 
