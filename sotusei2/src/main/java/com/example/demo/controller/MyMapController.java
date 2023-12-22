63ed6464a980fc8d06270e8c31ef011ac257e763
ikura99 <takahashirinto220@gmail.com>
2023-12-22 15:15:03 +0900

    12/22

--- sotusei2/src/main/java/com/example/demo/controller/MyMapController.java
+++ sotusei2/src/main/java/com/example/demo/controller/MyMapController.java
@@ -1,10 +1,7 @@
 package com.example.demo.controller;
 
-import java.util.Map;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,7 +19,7 @@ public class MyMapController {
 	
 	
 //開始地点、中継地点、終了地点完成
-//いじらない
+//いじらない!!!!!!!!!!!!!!!!!!
 	@RequestMapping(path = "/mymap2", method = RequestMethod.GET)
 	public String prepre() {
 		return "prepre2";
@@ -59,27 +56,40 @@ public class MyMapController {
 
 
 
-//失敗 中継地点の追加
-//一応残してる
-	@RequestMapping(path = "/mymap3", method = RequestMethod.GET)
-	public String karikari() {
+//今いじってる
+//現在点の取得
+	@RequestMapping(path = "/mymap5", method = RequestMethod.GET)
+	public String mymap5() {
 		return "prepre";
 	}
-
-@RequestMapping(path = "/kaisi3", method = RequestMethod.POST)
-public String karikari2(String start, String goal,Model model, @RequestBody Map<String, String> formData) {
-	model.addAttribute("start", start);	
-	model.addAttribute("goal", goal);	
-	model.addAttribute("formData", formData);
-	System.out.println(start+goal);
-	return "renshuu4";
+	@RequestMapping(path = "/kaisi5", method = RequestMethod.POST)
+	public String kaisi5(
+	    String start,
+	    String goal,
+	    @RequestParam(value = "relay[]", required = false) String[] relayPoints,
+	    Model model) {
+		//required = falseでrelayPointsの中身が無くてもエラーが起きないようにしている
+		model.addAttribute("start", start);	
+		model.addAttribute("goal", goal);	
+			
+		System.out.println(start);
+		System.out.println(goal);
+		
+		if (relayPoints != null) {
+			for (int i = 0; i < relayPoints.length; i++) {
+	            System.out.println("Relay Point " + (i + 1) + ": " + relayPoints[i]);
+	        }
+			model.addAttribute("relayPoints", relayPoints);
+	    }
+		
 	
-}
+		return "renshuu4";
+	}
 
 
 
 //開始地点と終了地点の検索完成
-//いじらないやつ
+//いじらないやつ-----------------------------。
 @RequestMapping(path = "/mymap4", method = RequestMethod.GET)
 public String karikari8888() {
 	return "renshuu6";
